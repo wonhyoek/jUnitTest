023114c4984acf2824f27f4d3e3a9fb80839b50b
wonhyoek <epahsejtmxm@naver.com>
2022-11-07 00:47:16 +0900

    first setting

---
+++ src/test/java/com/wh/jUnit/JUnitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wh.jUnit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JUnitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
